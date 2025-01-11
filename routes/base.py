03e9a0d31eef4eb2085ef420595b59b48dd5c57b
fatnaoui <hamzafatnaoui.ai@gmail.com>
2025-01-12 00:44:05 +0100

    testing

--- routes/base.py
+++ routes/base.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
+import os
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"] 
+)
 
 @base_router.get("/")
 def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
     return {
-        "key":"Hello World!"
+        "app_name":app_name,
+        "app_version":app_version
     }
+
